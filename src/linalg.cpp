6d7e3262005db538447646706468f7b7b9e16378
Thibaut D <thibaut.desauty@gmail.com>
2026-04-05 09:01:04 -0500

    Matrix-Vector Multiplication

--- src/linalg.cpp
+++ src/linalg.cpp
@@ -1,11 +1,32 @@
+#include <iostream>
+#include <vector>
+
+void multiply_mv_row_major(const double* matrix, int rows, int cols, const double* vector, double* result)
+{
+    for (int i = 0; i < rows; ++i)
+    {
+        result[i] = 0;
+        for (int j = 0; j < cols; ++j)
+        {
+            result[i] += matrix[i * cols + j] * vector[j];
+
+        };
+    };
+};
 
 
+void multiply_mv_col_major(const double* matrix, int rows, int cols, const double* vector, double* result)
+{
+    for (int i = 0; i < rows; ++i)
+    {
+        result[i] = 0;
+        for (int j = 0; j < cols; ++j)
+        {
+            result[i] += matrix[i + j * rows] * vector[j];
 
-void multiply_mv_row_major(const double* matrix, int rows, int cols, const double* vector, double* result);
-
-
-void multiply_mv_col_major(const double* matrix, int rows, int cols, const double* vector, double* result);
-
+        };
+    };
+};
 
 void multiply_mm_naive(const double* matrixA, int rowsA, int colsA, const double* matrixB, int rowsB, int colsB, double* result);
 
